35e1bb40070cd7f079a0192892c6b9d843b63b91
HenryBarbie <42897616+HenryBarbie@users.noreply.github.com>
2018-09-02 18:14:37 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+let button = document.querySelector("#login");
+
+button.addEventListener('click', function() {
+    prompt("Usuário:"), prompt("Senha:")
+})
+
+let cadastro = document.querySelector("#cadastro");
+
+cadastro.addEventListener('click', function() {
+    prompt("Nome Completo:"), prompt("Número da Carteira Nacional de Saúde"), prompt("E-mail:"), prompt("Senha:")
+})
+
+
+
